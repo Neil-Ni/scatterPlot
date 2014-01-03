ce0c6c3d91b854ee378410cb5f23a5d6dcd28db0
Neil-Ni <tzuyangni@gmail.com>
2014-01-03 16:44:59 +0800

    fixed spacing

--- client/js/controllers/main.js
+++ client/js/controllers/main.js
@@ -1,7 +1,6 @@
 'use strict';
 
 var app = angular.module('scatterPlotApp');
-
 app.controller('MainCtrl', function ($scope, $http, StockModel) {
   $scope.stockData = StockModel;
 });
--- client/js/services.js
+++ client/js/services.js
@@ -3,11 +3,11 @@
 var services = angular.module('scatterPlotApp.services', [])
 
 services.factory('StockModel', function ($http) {
-    var obj = {content:null};
-    $http.get('/static/scatterPlot/stocks.json').success(function(data) {
-        obj.content = data;
-    });    
-    return obj;  
+  var obj = {content:null};
+  $http.get('/static/scatterPlot/stocks.json').success(function(data) {
+    obj.content = data;
+  });    
+  return obj;  
 });
 
 
